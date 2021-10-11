15763a84a1063b34deeb2d2d9972a1c8caf4c9c9
Pubadee Klinocha <poom90914@gmail.com>
2021-10-11 10:42:58 +0700

    Configure tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint no-use-before-define: 0 */  // --> OFF
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
